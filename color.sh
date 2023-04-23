d84f161a2f5004775f90500bc5a74837e8a304ad
slvler <56368718+slvler@users.noreply.github.com>
2023-04-23 14:44:28 +0300

    Add files via upload

---
+++ color.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+NC=$'\e[0m'
+
+# Background
+REDBg=$'\e[0;41m'
+GREENBg=$'\e[0;42m'
+YELLOWBg=$'\e[0;43m'
+BLUEBg=$'\e[0;44m'
+PINKBg=$'\e[0;45m'
+CYANBg=$'\e[0;46m'
+WHITEBg=$'\e[0;47m'
+
+
+# Color
+RED=$'\e[0;31m'
+GREEN=$'\e[0;32m'
+YELLOW=$'\e[0;33m'
+BLUE=$'\e[0;34m'
+PINK=$'\e[0;35m'
+CYAN=$'\e[0;36m'
+WHITE=$'\e[0;37m'
+
+
+# Underline
+URED=$'\e[4;31m'
+UGREEN=$'\e[4;32m'
+UYELLOW=$'\e[4;33m'
+UBLUE=$'\e[4;34m'
+UPINK=$'\e[4;35m'
+UCYAN=$'\e[4;36m'
+UWHITE=$'\e[4;37m'
+
+
+
+
+echo "${RED} TEXT ${NC}"
+echo "${GREEN} TEXT ${NC}"
+echo "${YELLOW} TEXT ${NC}"
+echo "${BLUE} TEXT ${NC}"
+echo "${PINK} TEXT ${NC}"
+echo "${CYAN} TEXT ${NC}"
+echo "${WHITE} TEXT ${NC}"
+
+echo "${REDBg} TEXT ${NC}"
+echo "${GREENBg} TEXT ${NC}"
+echo "${YELLOWBg} TEXT ${NC}"
+echo "${BLUEBg} TEXT ${NC}"
+echo "${PINKBg} TEXT ${NC}"
+echo "${CYANBg} TEXT ${NC}"
+echo "${WHITEBg} TEXT ${NC}"
+
+
+echo "${URED} TEXT ${NC}"
+echo "${UGREEN} TEXT ${NC}"
+echo "${UYELLOW} TEXT ${NC}"
+echo "${UBLUE} TEXT ${NC}"
+echo "${UPINK} TEXT ${NC}"
+echo "${UCYAN} TEXT ${NC}"
+echo "${UWHITE} TEXT ${NC}"
