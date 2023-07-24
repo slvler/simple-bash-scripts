96581f32e5f14b9886d246f7e535253fda61d498
slvler <56368718+slvler@users.noreply.github.com>
2023-07-24 23:50:34 +0300

    Update read-function.sh

--- read-function.sh
+++ read-function.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 
+# string output
+# read
+# function output
+
+echo $1 $2 $3
+
+args=("$@")
+#echo ${args[0]} ${args[1]} ${args[2]}
+echo $@ / dinamic arguments
+echo $# / variable count
 
 printf "Year: "
-
 read year
-
 echo $year
