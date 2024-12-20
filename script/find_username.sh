cc6c684bc36deee50fa14f34e17dd7acd6fe9ab8
slvler <hselviler1@gmail.com>
2024-12-20 20:06:26 +0300

    update

--- script/find_username.sh
+++ script/find_username.sh
@@ -7,42 +7,61 @@ if [[ $# -ne 0 ]]; then
   exit 1
 fi
 
-github()
-{
-    baseurl="https://github.com"
-    result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
+github(){
+baseurl="https://github.com"
+result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
 
-    if [ "$result" == "200" ]; then
-        echo "Success: HTTP 200"
-    else
-        echo "Failure: HTTP $result"
-    fi
+if [ "$result" == "200" ]; then
+    echo "Success: HTTP 200"
+else
+    echo "Failure: HTTP $result"
+fi
 }
 
-reddit()
-{
-    baseurl="https://www.reddit.com/user/"
-    result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
+reddit(){
+baseurl="https://www.reddit.com/user/"
+result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
 
-    if [ "$result" == "200" ]; then
-        echo "Success: HTTP 200"
-    else
-        echo "Failure: HTTP $result"
-    fi
+if [ "$result" == "200" ]; then
+    echo "Success: HTTP 200"
+else
+    echo "Failure: HTTP $result"
+fi
 }
 
-devto()
-{
-    baseurl="https://dev.to"
-    result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
+devto(){
+baseurl="https://dev.to"
+result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
 
-    if [ "$result" == "200" ]; then
-        echo "Success: HTTP 200"
-    else
-        echo "Failure: HTTP $result"
-    fi
+if [ "$result" == "200" ]; then
+    echo "Success: HTTP 200"
+else
+    echo "Failure: HTTP $result"
+fi
+}
+
+medium(){
+baseurl="https://medium.com/@"
+result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
+
+if [ "$result" == "200" ]; then
+    echo "Success: HTTP 200"
+else
+    echo "Failure: HTTP $result"
+fi
+}
+
+youtube(){
+baseurl="https://www.youtube.com/@"
+result=$(curl -I "$baseurl/$username" | grep -w "HTTP" | awk '{print $2}')
+
+if [ "$result" == "200" ]; then
+    echo "Success: HTTP 200"
+else
+    echo "Failure: HTTP $result"
+fi
 }
 read -p "Enter to username: " username
 #github $username
 #reddit $username
-devto $username
+youtube $username
